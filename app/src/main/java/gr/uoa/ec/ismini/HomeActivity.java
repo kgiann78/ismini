b6aaa0729b9f0265cf6ff6cfcfb0024b2b7ad5c5
kgiann78 <kgiann78@gmail.com>
2016-06-22 21:37:49 +0300

    Login and Create Account activities. HomeActivity now contains a ListView to display products or services

--- app/src/main/java/gr/uoa/ec/ismini/HomeActivity.java
+++ app/src/main/java/gr/uoa/ec/ismini/HomeActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -18,7 +19,7 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        Button fab = (Button) findViewById(R.id.ButtonViewCart);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
